97616e1d07cf2c39aaaf6ca5169e36a3d3bce352
Taizo Curry <taizoc@gmail.com>
2022-11-10 19:01:40 -0800

    Wowowowow this is the first time I've made changes to code and committed to github NOT uploading a text file thru a web browser

--- nvmlLibraryMismatch.sh
+++ nvmlLibraryMismatch.sh
@@ -1,35 +1,32 @@
-#===
-#v0.3
 #!/bin/bash
-#requirements 
-#this is mostly comments, isn't it...
-#for testing, watch lsof nvidia*
-#get processes utilizing nvidia driver units
+#v0.3.1
+#features to add: 
+##detect and stop services envoking any processes touching nvidia driver units. (is that wording right?) 
+##requirements for specific OS version? 
+#this thing is mostly comments, isn't it... 
+##for testing, watch lsof /dev/nvidia* 
+
+#get processes utilizing nvidia driver units 
 procs=$(sudo lsof -w /dev/nvidia* |cut -f1 -d ' ')
 
 #stop lightdm, bc that's DEF the only thing using nv drivers :O
 sudo service lightdm stop
+##this will throw an error if service doesn't exist... but that seems to be OK. 
 #loop to kill all processes using NV drivers
 for proc in $procs
 do
-#while [ $proc != "COMMAND" ] ##looks like pkill will not complain about trying to kill ill-defined procs :3
-#while [ 0=0 ]
-##maybe test command instead of != 
-#do
+#while [ $proc != "COMMAND" ]
+#do 
   sudo pkill -f $proc
+  ##looks like pkill will not complain about trying to kill ill-defined procs "COMMAND" :3 
 done
-
-#after processes are killed...
+#after processes are killed... get names of NV driver units to rmmod  
 mods=$(lsmod |grep nvidia |cut -f1 -d ' ')
 for mod in $mods
 do
  sudo rmmod "$mod"
 done
 
-#restart lightdm service
+#start lightdm service
 sudo service lightdm start
-#done
-
-#sudo service lightdm start
-#successfully kills them when no error. If error, however, need to kill any running apps using them.
-#===
+##will also throw error but not matter if service doesn't exist on the system. 
